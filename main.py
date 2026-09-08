b02da219a26a3208731b160ce1d22b078d0fa7bf
kp0430 <favabean12@gmail.com>
2026-09-08 17:45:50 -0500

    limit frame rate to 60 fps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
         
 
 if __name__ == "__main__":
